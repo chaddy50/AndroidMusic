03a331a7cec457cf35bc6a982a68c3c5f952a794
Nathan <nchaddy50@protonmail.com>
2024-04-04 18:39:19 -0500

    Fix issue with spacing in Genres tab

--- app/src/main/java/com/chaddy50/musicapp/views/Genres.kt
+++ app/src/main/java/com/chaddy50/musicapp/views/Genres.kt
@@ -1,22 +1,32 @@
 package com.chaddy50.musicapp.views
 
 import android.content.Context
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.chaddy50.musicapp.components.EntityCard
+import com.chaddy50.musicapp.components.TopBar
 import com.chaddy50.musicapp.data.MusicDatabase
 import com.chaddy50.musicapp.navigation.Screen
 
 @Composable
 fun Genres(context: Context, musicDatabase: MusicDatabase, navController: NavController) {
-    LazyColumn {
-        items(musicDatabase.genres.toList()) { genre ->
-            EntityCard(
-                genre.title,
-                { navController.navigate(Screen.ArtistScreen.route + "?genreID=${genre.id}") }
-            )
+    Scaffold(
+            topBar = { TopBar(0, "Genres") }
+            ) {
+        LazyColumn (
+            Modifier.padding(it)
+        ){
+            items(musicDatabase.genres.toList()) { genre ->
+                EntityCard(
+                    genre.title,
+                    { navController.navigate(Screen.ArtistScreen.route + "?genreID=${genre.id}") }
+                )
+            }
         }
     }
 }
