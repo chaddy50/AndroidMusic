a898f1c332f23c530f940e46cad7c7db7e0b19cf
chaddy50 <nchaddy50@protonmail.com>
2025-10-14 20:35:32 -0500

    Show album year in card

--- app/src/main/java/com/chaddy50/musicapp/components/cards/AlbumCard.kt
+++ app/src/main/java/com/chaddy50/musicapp/components/cards/AlbumCard.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -45,17 +47,21 @@ fun AlbumCard(
             ) {
                 Column{
                     album.artwork?.asImageBitmap()
-                        ?.let { Image(bitmap = it, contentDescription = "Album artwork") }
+                        ?.let { Image(
+                            bitmap = it,
+                            contentDescription = "Album artwork",
+                            modifier = Modifier
+                                .aspectRatio(1f)
+                        ) }
                 }
                 Column(modifier = Modifier.padding(10.dp, 0.dp)) {
-                    Row {
-                        Text(album.title, style = TextStyle(fontSize = 16.sp))
-                    }
+                    Text(album.title, style = TextStyle(fontSize = 16.sp))
+
                     if (shouldShowArtist) {
-                        Row {
                             Text(album.artist, style= TextStyle(fontSize = 14.sp))
-                        }
                     }
+
+                    Text(album.year, style = TextStyle(fontSize = 14.sp))
                 }
             }
         }
